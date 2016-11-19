161ef263006a7b552cc828f5eb4bd8b1e6b3986a
Eugene Gundorov <e.n.gundorov@gmail.com>
2016-11-20 01:03:43 +0300

    Titles.

--- src/components/AboutPage/index.tsx
+++ src/components/AboutPage/index.tsx
@@ -4,12 +4,17 @@ import './AboutPage.scss';
 
 import Page from '../Page';
 
-export default function AboutPage (props) {
-    console.log(props);
-    return (
-        <Page>
-            <h1>Hello there!</h1>
-            <p>My name is Eugene Gundorov. I'm a frontend developer from Kazan, Russia.</p>
-        </Page>
-    )
+export default class AboutPage extends React.Component<void, void> {
+    componentDidMount () {
+        document.title = 'About me - Eugene Gundorov aka cy6eria'
+    }
+    
+    render () {
+        return (
+            <Page>
+                <h1>Hello there!</h1>
+                <p>My name is Eugene Gundorov. I'm a frontend developer from Kazan, Russia.</p>
+            </Page>
+        )
+    }
 }
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -4,23 +4,29 @@ import * as React from 'react';
 
 import MainNavigation from '../MainNavigation';
 
-export default function MainPage () {
-    return (
-        <header>
-            <div className="image"/>
-            <div className="content">
-                <div className="left-col">
-                    <h1>Eugene Gundorov</h1>
-                    <h2>
-                        <span>developer</span>
-                        <span>  traveler</span>
-                        <span>  badass</span>
-                    </h2>
+export default class MainPage extends React.Component<void, void> {
+    componentDidMount () {
+        document.title = 'Home - Eugene Gundorov aka cy6eria'
+    }
+
+    render () {
+        return (
+            <header>
+                <div className="image"/>
+                <div className="content">
+                    <div className="left-col">
+                        <h1>Eugene Gundorov</h1>
+                        <h2>
+                            <span>developer</span>
+                            <span>  traveler</span>
+                            <span>  badass</span>
+                        </h2>
+                    </div>
+                    <div className="right-col">
+                        <MainNavigation align="right" />
+                    </div>
                 </div>
-                <div className="right-col">
-                    <MainNavigation align="right" />
-                </div>
-            </div>
-        </header>
-    );   
+            </header>
+        );   
+    }
 }
