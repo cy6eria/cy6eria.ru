1f4d570e6603edf436ad0b9b0b5f39969cbb1c38
Eugene Gundorov <e.n.gundorov@gmail.com>
2017-01-05 21:35:51 +0300

    At first, try to load file. In other case load index.html.

--- server.js
+++ server.js
@@ -10,20 +10,17 @@ function sendFile (response, filePath, fileContent) {
     response.end(fileContent);
 };
 
-function send404 (response) {
-    response.writeHead(404, {"Content-Type": "text/html"});
-    fs.readFile('./public/404.html', (err, data) => response.end(data));
-};
-
 const server = http.createServer((request, response) => {
-    let filePath = 'index.html';
-
-    if (['text/css', 'application/javascript'].indexOf(mime.lookup(path.basename(request.url))) >= 0) {
-        filePath = request.url
-    }
+    let filePath = request.url;
 
     fs.readFile(`./public/${filePath}`, (err, data) => {
-        !err ? sendFile(response, filePath, data) : send404(response);
+        if (err) {
+            fs.readFile(`./public/index.html`, (err, data) => {
+                sendFile(response, 'index.html', data);
+            })
+        } else {
+            sendFile(response, filePath, data);
+        }
     });   
 });
 
