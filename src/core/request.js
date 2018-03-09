912bce0dfe26cb4a01487eaeb43d881a1ce4dcab
Evgeny Gundorov <eugene@Evgenys-MacBook-Pro.local>
2018-03-09 19:44:05 +0300

    New request config.

--- src/core/request.js
+++ src/core/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const baseURL = process.env.npm_config_base_url || 'http://localhost:5100';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://cy6eria.ru' : 'http://localhost:5100';
 
 export const request = axios.create({ baseURL });
