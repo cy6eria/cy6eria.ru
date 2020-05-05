21d6d85abf1094753c0652028b1b49ce8bc2857b
Eugene Gundorov <e.n.gundorov@gmail.com>
2020-05-05 17:15:16 +0300

    Размер шрифта обычного текста уменьшин.

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+import './Layout.scss';
+
+import { Sidebar } from '../Sidebar';
+
+export const Layout: FC = React.memo((props) => (
+  <>
+    <div className="main">
+      {props.children}
+    </div>
+    <Sidebar />
+  </>
+));
