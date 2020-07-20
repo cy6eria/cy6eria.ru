81bf72af260fb4264d6b08ea93e338ea3115ac94
Eugene Gundorov <e.n.gundorov@gmail.com>
2020-07-20 09:36:52 +0300

    Packages versions are bumped.

--- src/core/history.js
+++
@@ -1,3 +0,0 @@
-import { createBrowserHistory } from 'history';
-
-export const history = typeof(window) !== 'undefined' ? createBrowserHistory() : {};
--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,2 @@
-export * from './history';
 export * from './date';
 export * from './request';
