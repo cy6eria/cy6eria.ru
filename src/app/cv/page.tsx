b516447d79a992e5944b41b2fa8617fcf3906cdc
Eugene Gundorov <e.n.gundorov@gmail.com>
2023-10-02 21:54:41 +0200

    CV text fixed.

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -25,7 +25,7 @@ const data = {
         value: 'cy6eria',
       }
     ],
-    summary: 'Experienced JavaScript Developer with 10+ years in the industry. Proficient with React. Created web banking apps with big corporations and real time data visualization tools in a startup way.',
+    summary: 'Experienced JavaScript Developer with 10+ years in the industry. Proficient with React. Created web banking apps with big corporations and real-time data visualization tools in a startup way.',
     workHistory: [
           {
             companyName: 'Telleqt PTE, LTD',
@@ -42,17 +42,17 @@ const data = {
             position: 'Lead Front-end developer',
             startDate: new Date(2021, 5),
             endDate: new Date(2023, 1),
-            description: 'Multibonus is a virtual currency which our clients can convert in real money or spend in our marketplace.',
+            description: 'Multibonus is a virtual currency our clients can convert into real money or spend in our marketplace.',
             responsibilities: `
-* Initial estimation of epics as a representative of the front end team;
-* Collaboration with analytics during development processes to confirm feature proposals and find a best solution;
+* Initial estimation of epics as a representative of the front-end team;
+* Collaboration with analytics during development processes to confirm feature proposals and find the best solution;
 * Managing tasks among front-end developers, control progress, responsibility for code, support release;
 * Participation in team activities (Spring planning, review, retro);
 * Development, code review, unit testing;
             `,
             achievements: `
 * Increased involvement of developers in the discussion team sprint results;
-* Initiated and support migration to more standard stack to make hire process easy;
+* Initiated and supported migration to a more standard stack to make a hiring process easy;
             `,
             technologies: ['React', 'Typescript', 'Reatom', 'Effector', 'Styled Components', 'Webpack', 'Agile', 'git'],
         },
@@ -62,7 +62,7 @@ const data = {
             position: 'Full-stack developer, Lead Front-end developer',
             startDate: new Date(2018, 3),
             endDate: new Date(2021, 5),
-            description: 'The company is focused on the IT solutons for the oil production business.',
+            description: 'The company focused on IT solutions for the oil production business.',
             responsibilities: `
 * Collaboration with stakeholders to launch projects from scratch, choosing technologies, providing design consulting;
 * Managing tasks among front-end developers, control progress, responsibility for code, support release;
@@ -72,7 +72,7 @@ const data = {
             `,
             achievements: `
 * Successfully launched 3 MVP projects;
-* Integrated few junior front end developers in the production process of launched projects;
+* Integrated few junior front-end developers in the production process of launched projects;
             `,
             technologies: ['React', 'Typescript', 'Redux', 'SCSS', 'Styled Components', 'Webpack', 'REST', 'GraphQL (Apollo)', 'Node.js', 'Docker', 'git'],
         },
@@ -82,14 +82,14 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2017, 9),
             endDate: new Date(2018, 3),
-            description: 'Ak Bars is a biggest bank in my home region. I was a part of retail clients web app.',
+            description: 'Ak Bars is the biggest bank in my home region. I was a part of the retail client web app.',
             responsibilities: `
 * Collaboration with PO to discuss details of new features;
 * Participation in team activities (Spring planning, review, retro);
-* Develop the new features, code review, unit testing;`,
+* Develop the new features, code review, and unit testing;`,
             achievements: `
 * Successfully implemented all features of the old version of an app and launched beta as a release version;
-* Developed a self confidence as a lead developer;
+* Developed self-confidence as a lead developer;
 `,
             technologies: ['React', 'Redux', 'SCSS', 'Webpack', 'Agile', 'git'],
         },
@@ -99,10 +99,10 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2015, 2),
             endDate: new Date(2017, 9),
-            description: 'As a part of collaboration between IBM and a biggest Russian bank, we were developing a new web app for business customers from scratch.',
+            description: 'As a part of a collaboration between IBM and the biggest Russian bank, we were developing a new web app for business customers from scratch.',
             responsibilities: `
-* Participation in the process of forming technological stack at the initial stages of the project
-* Migration old features of the app to the new stack;
+* Participation in the process of forming a technological stack at the initial stages of the project
+* Migration of old features of the app to the new stack;
 * Collaboration with stakeholders to discuss details of new features, UI/UX;
 * Participation in team activities (Sprint planning, review, retro);
 * Development, code review, unit testing;
@@ -118,7 +118,7 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2014, 8),
             endDate: new Date(2015, 2),
-            description: 'This is a real estate startup and my first SPA project.',
+            description: 'It`s a real estate startup and my first SPA project.',
             responsibilities: `
 * Collaboration with stakeholders to discuss details of new features, UI/UX;
 * Development, code review;
@@ -132,13 +132,13 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2013, 5),
             endDate: new Date(2014, 8),
-            description: 'Flatstack is a outsourcing company who works with startups, mostly from US.',
+            description: 'Flatstack is an outsourcing company that works with startups, mostly from the US.',
             responsibilities: `
 * Participation in team activities (Spring planning, review, retro);
-* Creation templates for views according PSD mockups;
+* Creation templates for views according to PSD mockups;
 * Handling forms with JS code;
             `,
-            achievements: '* Launched few different projects, but most of them weren\'t successful (which typical situation for startups);',
+            achievements: '* Launched a few different projects, but most of them weren\'t successful (which typical situation for startups);',
             technologies: ['Ruby on Rails', 'Javascipt', 'Coffescript', 'JQuery', 'SCSS', 'git', 'Photoshop', 'Agile'],
         },
         {
@@ -147,13 +147,13 @@ const data = {
             position: 'Full-stack developer',
             startDate: new Date(2013, 0),
             endDate: new Date(2013, 5),
-            description: 'This company provided outsource service for small companies and startups.',
+            description: 'This company provides outsourced services for small companies and startups.',
             responsibilities: `
 * Development of REST API with Ruby on Rails;
-* Creation templates for views according PSD mockups;
+* Creation templates for views according to PSD mockups;
 * Handling forms with JS code;
             `,
-            achievements: '* Launched a promo site for Russian Lucky Strike comeback company;',
+            achievements: '* Launched a promo site for the Russian Lucky Strike comeback company;',
             technologies: ['Ruby on Rails', 'Javascipt', 'Coffescript', 'JQuery', 'SCSS', 'Photoshop'],
         },
         {
@@ -162,9 +162,9 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2012, 8),
             endDate: new Date(2012, 11),
-            description: 'This was my first full-time job after year in army. The project was a CRM system for a microfinance company.',
+            description: 'It was my first full-time job after a year in the army. The project was a CRM system for a microfinance company.',
             responsibilities: `
-* Creation of templates for views according PSD mockups;
+* Creation of templates for views according to PSD mockups;
 * Handling forms with JS code;
             `,
             technologies: ['Twig', 'JQuery', 'git', 'Photoshop'],
@@ -175,8 +175,8 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2011, 2),
             endDate: new Date(2011, 6),
-            description: 'Another part-time job during my university course. Funbox is B2B company for cellular operators.',
-            responsibilities: '* Development of promo pages for new products and events according PSD mockups;',
+            description: 'An another part-time job during my university course. Funbox is a B2B company for cellular operators.',
+            responsibilities: '* Develop of promo pages for new products and events according to PSD mockups;',
             achievements: '* Became familiar with new technologies (Ruby on Rails, Git);',
             technologies: ['HAML', 'JQuery', 'Less', 'git', 'Photoshop'],
         },
@@ -186,7 +186,7 @@ const data = {
             position: 'Front-end developer',
             startDate: new Date(2010, 5),
             endDate: new Date(2011, 2),
-            description: 'This is start of my carrier. It was part-time job during my university course.',
+            description: 'It`s the start of my carreer. It was a part-time job during my university course.',
             responsibilities: `
 * Development promo pages and e-shops for local companies;
 * Setup Joomla CMS in the production environment;
