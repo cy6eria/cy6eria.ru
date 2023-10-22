6532b4c8494ce4809c54284221fe0e76bbb289c6
Eugene Gundorov <e.n.gundorov@gmail.com>
2023-10-22 23:40:14 +0200

    Bump.

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -3,11 +3,6 @@ import { CV } from '@components';
 import * as es from './es';
 import * as en from './en';
 
-const variants = {
-  es,
-  en,
-};
-
 export default function CVPage () {  
   return (
     <CV
