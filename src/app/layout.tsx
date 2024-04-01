1c62d02653db774fca356d66e5325dbcdb8b6f7c
Eugene Gundorov <e.n.gundorov@gmail.com>
2024-04-01 20:46:21 +0200

    Loader added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         {children}
 
-        {!isDev && <YandexMetrika />}
+        {!isDev && <YandexMetrika />} 
       </body>
     </html>
   );
